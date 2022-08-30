dacec535b6d2675c67705d59f5194ed440ffc308
Cosine <sak9188@163.com>
2022-08-31 01:46:38 +0800

    添加验证层相关代码

--- source/runtime/RenderInterface.cpp
+++ source/runtime/RenderInterface.cpp
@@ -1,14 +1,17 @@
 #include "RenderInterface.h"
 
 #include <iostream>
-#include <vector>
+#include <cstring>
 
 namespace solis
 {
+    const std::vector<const char*> RenderInterface::validationLayers = {
+        "VK_LAYER_KHRONOS_validation" 
+    };
+
     RenderInterface::RenderInterface()
     {
         // 初始化Vulkan
-
         // VKAppInfo
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -24,15 +27,29 @@ namespace solis
         createInfo.pApplicationInfo = &appInfo;
 
         // Vulkan 扩展
-        uint32_t glfwExtensionCount = 0;
-        const char **glfwExtensions;
+        auto&& extensions = getRequiredExtensions();
+        std::cout << "after move 0:" << &extensions << "\n";
+        std::cout << "after move 1:" << static_cast<void*>(extensions.data()) << "\n";
 
-        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
-        createInfo.enabledExtensionCount = glfwExtensionCount;
-        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+        createInfo.ppEnabledExtensionNames = extensions.data();
 
-        // 全局验证层
-        createInfo.enabledLayerCount = 0;
+        // Vulkan 全局验证层
+        if(validationEnable())
+        {
+            if (!checkValidationLayerSupport())
+            {
+                throw std::runtime_error("validation layers required, bug not available!");
+            }
+
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+
+        }
+        else
+        {
+            createInfo.enabledLayerCount = 0;
+        }
 
         // 导致VK创建失败
         VkResult ret = vkCreateInstance(&createInfo, nullptr, &instance_);
@@ -41,24 +58,77 @@ namespace solis
             throw std::runtime_error("failed to create instance");
         }
 
-        // 在这里检查一下支持哪些扩展
-        uint32_t extensionCount{0};
-        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-
-        std::vector<VkExtensionProperties> extensions(extensionCount);
-        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
-
-        std::cout << "available extensions:" << extensionCount
-                  << std::endl;
-        for (const auto &extension : extensions)
-        {
-            std::cout << '\t' << extension.extensionName << '\n';
-        }
     }
 
     RenderInterface::~RenderInterface()
     {
         std::cout << "render interface over \n"; 
         vkDestroyInstance(instance_, nullptr);
+    }
+
+    std::vector<const char*>&& RenderInterface::getRequiredExtensions()
+    {
+        // 在这里检查一下支持哪些扩展
+        uint32_t extensionCount{0};
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> extensionsProperties(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensionsProperties.data());
+
+        std::cout << "available extensions:" << extensionCount
+                  << std::endl;
+        for (const auto& extension : extensionsProperties)
+        {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+
+
+        // 这里载入需要的扩展
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        std::vector<const char*> extensions(glfwExtensions, glfwExtensions + glfwExtensionCount);
+
+        if(validationEnable())
+        {
+            extensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        }
+
+        // 这里检验一下是否发生移动了
+        std::cout << "before move 0:" << &extensions << "\n";
+        std::cout << "before move 1:" << static_cast<void*>(extensions.data()) << "\n";
+
+        return std::move(extensions);
+    }
+
+    bool RenderInterface::checkValidationLayerSupport()
+    {
+        uint32_t layerCount;
+
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+        std::vector<VkLayerProperties> avaliableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, avaliableLayers.data());
+
+        std::cout << "available layers:" << layerCount
+                  << std::endl;
+        for (const auto &layer: avaliableLayers)
+        {
+            std::cout << '\t' << layer.layerName<< '\n';
+        }
+
+        for(const char* layerName : validationLayers)
+        {
+            for(const auto& layerProperties : avaliableLayers) 
+            {
+                if(strcmp(layerName, layerProperties.layerName) == 0)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
--- source/runtime/RenderInterface.h
+++ source/runtime/RenderInterface.h
@@ -3,19 +3,43 @@
 
 #define GLFW_INCLUDE_VULKAN
 #include "GLFW/glfw3.h"
-
 #include "Config.h"
+
+#include <vector>
 
 namespace solis
 {
+    // RHI 渲染硬件接口
     class SOLIS_API RenderInterface
     {
     public:
         RenderInterface();
 
         virtual ~RenderInterface();
+
+
     private:
+        static const std::vector<const char*> validationLayers;
+
         VkInstance instance_;
+
+        // 获得层的扩展
+        std::vector<const char*>&& getRequiredExtensions();
+
+        // 是否开启验证层
+        constexpr bool validationEnable() 
+        {
+            #ifdef VK_VALIDATION_REQURED
+                return true;
+            #else:
+                return false;
+            #endif
+        }
+
+        // 检查验证层是否支持
+        bool checkValidationLayerSupport();
+
+        
     };
 } // namespace solisc
 
